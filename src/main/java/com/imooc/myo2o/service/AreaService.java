b9bbcf563ebdb49c755035df4898ad6ab0d21daf
qdp_glb <645792821@qq.com>
2018-08-20 12:19:07 +0800

    o2o first commit

---
+++ src/main/java/com/imooc/myo2o/service/AreaService.java
@@ -0,0 +1,9 @@
+package com.imooc.myo2o.service;
+
+import java.util.List;
+
+import com.imooc.myo2o.entity.Area;
+
+public interface AreaService {
+	List<Area> getAreaList();
+}
---
+++ src/main/java/com/imooc/myo2o/web/superadmin/AreaController.java
@@ -0,0 +1,26 @@
+package com.imooc.myo2o.web.superadmin;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.imooc.myo2o.entity.Area;
+import com.imooc.myo2o.service.AreaService;
+
+@Controller
+@RequestMapping("/superadmin")
+public class AreaController {
+	
+	@Autowired
+	AreaService areaService;
+	
+	@RequestMapping("/area")
+	@ResponseBody
+	public List<Area> getAreaList(){
+		List<Area> list = areaService.getAreaList();
+		return list;
+	}
+}
